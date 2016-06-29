c94b9800baa91a5f911e5840556621d2a72ea1cf
MadPsymon <simon81186@aol.com>
2016-06-29 17:41:39 +0200

    first commit

---
+++ DeTaCo/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeTaCo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //CSV-File Spaltenweise einlesen
+            string file = @"C:\Users\simon\Documents\workspace\DeTaCo\DeTaCo\test.csv";
+
+            StreamReader reader = new StreamReader(File.OpenRead(file));
+            List<rule> rules = new List<rule>();
+            while (!reader.EndOfStream)
+            {
+
+                var line = reader.ReadLine();
+                string[] values = line.Split(',');
+                if (rules.Count == 0)
+                {
+                    for (int i = 0; i < values.Length; i++)
+                    {
+                        rules.Add(new rule());
+                        rules[i].name = values[i];
+                    }
+                }
+                if (values[0] == "J" || values[0] == "N")
+                {
+                    for (int i = 0; i < rules.Count; i++)
+                    {
+                        rules[i].conditions.Add(values[i]);
+                    }
+                }
+                else if (!values[0].Contains("R"))
+                {
+                    for (int i = 0; i < rules.Count; i++)
+                    {
+                        rules[i].actions.Add(values[i]);
+                    }
+                }        
+            }
+            foreach (rule rule in rules)
+            {
+                rule.Output();
+            }
+            
+            Console.ReadLine();
+        }
+    }
+}
---
+++ DeTaCo/rule.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeTaCo
+{
+    class rule
+    {
+        public string name;
+        public List<string> conditions;
+        public List<string> actions;
+        
+        public rule()
+        {
+            name = "";
+            conditions = new List<string>();
+            actions = new List<string>();
+        }
+
+        //reine Test-Funktion zur Ausgabe der Werte
+        public void Output()
+        {
+            Console.WriteLine(this.name);
+            foreach (string condition in conditions)
+            {
+                Console.WriteLine(condition);
+            }
+            foreach (string action in actions)
+            {
+                Console.WriteLine(action);
+            }
+            Console.WriteLine();
+        }
+            
+    }
+}
